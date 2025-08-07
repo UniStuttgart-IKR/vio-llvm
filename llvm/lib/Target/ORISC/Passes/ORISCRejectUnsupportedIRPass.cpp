8eeec191b0759fa2d197a4e9ec99e44c52fec208
Jazz <leylajazz7@gmail.com>
2025-08-07 14:27:16 +0200

    first draft of ll rejector (is a bit harsh, but thats fine for now)

--- llvm/lib/Target/ORISC/Passes/ORISCRejectUnsupportedIRPass.cpp
+++ llvm/lib/Target/ORISC/Passes/ORISCRejectUnsupportedIRPass.cpp
@@ -1,13 +1,17 @@
 #include "Passes/ORISCRejectUnsupportedIRPass.h"
 #include "llvm/Analysis/InstructionSimplify.h"
+#include "llvm/IR/Analysis.h"
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/IR/DiagnosticInfo.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Support/Casting.h"
+#include "llvm/Support/WithColor.h"
+#include <cstdlib>
 #include <string>
 
 using namespace llvm;
@@ -16,6 +20,7 @@ static void emitError(Instruction &I, std::string Message) {
     I.getContext().diagnose(DiagnosticInfoUnsupported(
         *I.getFunction(), Message, I.getDebugLoc(), llvm::DS_Error
     ));
+    exit(1);
 }
 
 static void emitWarning(Instruction &I, std::string Message) {
@@ -24,6 +29,8 @@ static void emitWarning(Instruction &I, std::string Message) {
     ));
 }
 
+//We assume top to botton iteration, so the pointer conversion result must be
+//located higher to the icmp
 PreservedAnalyses RejectUnsupportedIRPass::run(Function &F, FunctionAnalysisManager &AM){
     DL = F.getParent()->getDataLayout();
     for (Instruction &I : instructions(F)){
@@ -48,12 +55,27 @@ void RejectUnsupportedIRPass::visitPointerConversion(Instruction *I){
         RemoveFromParentList.push_back(I);
         return;
     }*/
+    bool Clean = true;
+    for (User *U : I->users()){
+        if (!isa<ICmpInst>(U)) {
+            Clean = false;
+            break;
+        }
+        ICmpInst *ICmp = cast<ICmpInst>(U);
+        unsigned OpNum = I == ICmp->getOperand(0) ? 0 : 1;
+        ICmp->setOperand(OpNum, I->getOperand(0));
+    }
+
+    if (Clean) {
+        RemoveFromParentList.push_back(I);
+        return;
+    }
     
     if (isa<PtrToIntInst>(I))
         emitError(*I, "Casting Pointers to Integers is not supported by the Objective-RISC Target");
     else
         emitError(*I, "Casting Integers to Pointers is not supported by the Objective-RISC Target");
-    report_fatal_error("unsupported pointer ↔ integer conversion");
+    report_fatal_error("unsupported pointer ↔ integer conversion", false);
 }
 
 void RejectUnsupportedIRPass::visitPointerCompare(ICmpInst *ICmp){
@@ -63,8 +85,13 @@ void RejectUnsupportedIRPass::visitPointerCompare(ICmpInst *ICmp){
     Type *RTy = RHS->getType();
     ICmpInst::Predicate Pred = ICmp->getPredicate();
 
-    bool LIsPtr = LTy->isPointerTy();
-    bool RIsPtr = RTy->isPointerTy();
+    bool LIsCastedPtr = isa<IntToPtrInst>(LHS);
+    bool RIsCastedPtr = isa<IntToPtrInst>(LHS);
+    bool LIsCastedInt = isa<PtrToIntInst>(LHS);
+    bool RIsCastedInt = isa<PtrToIntInst>(LHS);
+
+    bool LIsPtr = (LTy->isPointerTy() || LIsCastedPtr) && !LIsCastedInt;
+    bool RIsPtr = (RTy->isPointerTy() || RIsCastedPtr) && !RIsCastedInt;
 
     //Pointer vs Integer: warn for EQ and NEQ, else error
     if (LIsPtr != RIsPtr) {
@@ -93,5 +120,5 @@ void RejectUnsupportedIRPass::visitPointerCompare(ICmpInst *ICmp){
     }*/
 
     emitError(*ICmp, "Ordered Compares (<, <=, >, >=) using Pointers are illegal for the Objective-RISC Target");
-    report_fatal_error("ordered pointer comparison not supported");
+    report_fatal_error("ordered pointer comparison not supported", false);
 }

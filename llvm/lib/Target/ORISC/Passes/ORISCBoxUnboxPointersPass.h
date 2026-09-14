7583fd47a31cac483a330bb38b94fe083383801b
Jazz <leylajazz7@gmail.com>
2026-09-14 12:48:34 +0200

    Started considering MemCpy (and firends) in BoxUnboxPass

--- llvm/lib/Target/ORISC/Passes/ORISCBoxUnboxPointersPass.h
+++ llvm/lib/Target/ORISC/Passes/ORISCBoxUnboxPointersPass.h
@@ -1,12 +1,14 @@
 #ifndef LLVM_LIB_TARGET_ORISC_ORISCBOXUNBOXPOINTERSPASS_H
 #define LLVM_LIB_TARGET_ORISC_ORISCBOXUNBOXPOINTERSPASS_H
 
+#include "llvm/ADT/MapVector.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/PassManager.h"
+#include <cstdint>
 #include <utility>
 
 namespace llvm {
@@ -18,6 +20,7 @@ public:
 
 private:
     typedef std::pair<Value *, Value *> FatPtr;
+    typedef std::pair<Value *, uint8_t> MemCpyMovArgument;
 
     FunctionCallee GepPFn;
     FunctionCallee GepIFn;
@@ -28,11 +31,13 @@ private:
     PointerType *BaseTy;
     PointerType *IndexTy;
     SmallVector<Instruction *> RemoveFromParentList = SmallVector<Instruction *>();
+    MapVector<Instruction *, MemCpyMovArgument> MemCpyMovMap = MapVector<Instruction *, MemCpyMovArgument>();
 
     bool visitPointerArgument(Value *A, Function *Parent);
     bool visitAlloc(Instruction *I);
     bool visitOther(Instruction *I);
     bool handleUser(Value *Base, Value *CurrentIndex, Value *Parent, User *U);
+    bool handleMemIntrinsics(Value *Parent, CallInst *Call);
 
     inline Value *createGep(IRBuilder<> *Builder, Value *Base, Value * CurrentIndex);
 
